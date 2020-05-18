8ca3dd7c804970a44b2f33e89a88916b07c10f86
Nico Sinisterra <nsinisterra@bondacom.com>
2020-05-17 22:04:41 -0300

    add spanish files

---
+++ modules/skin/data/locales/es.lua
@@ -0,0 +1,7 @@
+Translations = {
+  ['skin_menu'] = 'Menú de skin',
+  ['use_rotate_view'] = 'presiona ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ y ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ para girar la vista.',
+  ['skin'] = 'cambiar skin',
+  ['saveskin'] = 'guardar skin en un archivo',
+}
+ 
