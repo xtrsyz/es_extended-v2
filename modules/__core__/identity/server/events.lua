13fb065066cf5da0d31fee08c1e4bc7b2681f19c
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2020-05-26 05:10:24 +0200

    things

--- modules/__core__/identity/server/events.lua
+++ modules/__core__/identity/server/events.lua
@@ -1,4 +1,5 @@
 onRequest('esx:identity:check', function(source, cb)
+
   local player = xPlayer.fromId(source)
 
   MySQL.Async.fetchAll('SELECT first_name, last_name, dob FROM users WHERE identifier = @identifier', {['@identifier'] = player.identifier}, function(result)
@@ -27,13 +28,14 @@ end)
 onClient('esx:identity:register', function(firstName, lastName, dob, sex)
   local player = xPlayer.fromId(source)
 
-  MySQL.Sync.execute('UPDATE users SET first_name = @first_name, last_name = @last_name, dob = @dob, sex = @sex WHERE identifier = @identifier', {
-    ['@identifier'] 	= player.identifier,
-	  ['@first_name'] 	= firstName,
-	  ['@last_name'] 		= lastName,
-    ['@dob']	        = dob,
-    ['@sex']       		= sex
+  MySQL.Async.execute('UPDATE users SET first_name = @first_name, last_name = @last_name, dob = @dob, sex = @sex WHERE identifier = @identifier', {
+    ['@identifier'] = player.identifier,
+    ['@first_name'] = firstName,
+    ['@last_name']  = lastName,
+    ['@dob']        = dob,
+    ['@sex']        = sex
   })
+
 end)
 
 -- Extend xPlayer
@@ -75,33 +77,31 @@ on('esx:player:create', function(player)
 end)
 
 on('esx:player:serialize', function(player, add)
-  add({first_name = player:getFirstName()})
-  add({last_name = player:getLastName()})
-  add({dob = player:getDOB()})
-  add({sex = player:getSex()})
+
+  add({
+    first_name = player:getFirstName(),
+    last_name  = player:getLastName(),
+    dob        = player:getDOB(),
+    sex        = player:getSex()
+  })
+
 end)
 
 on('esx:player:serialize:db', function(player, add)
-  add({first_name = player:getFirstName()})
-  add({last_name = player:getLastName()})
-  add({dob = player:getDOB()})
-  add({sex = player:getSex()})
+
+  add({
+    first_name = player:getFirstName(),
+    last_name  = player:getLastName(),
+    dob        = player:getDOB(),
+    sex        = player:getSex()
+  })
+
 end)
 
 on('esx:player:load:first_name', function(identifier, playerId, row, userData, addTask)
 
   addTask(function(cb)
-
-    local data = {}
-
-    if row.first_name and row.first_name ~= '' then
-      data = row.first_name
-    else
-      data = {}
-    end
-
-    cb({first_name = data})
-
+    cb({first_name = row.first_name})
   end)
 
 end)
@@ -109,17 +109,7 @@ end)
 on('esx:player:load:last_name', function(identifier, playerId, row, userData, addTask)
 
   addTask(function(cb)
-
-    local data = {}
-
-    if row.last_name and row.last_name ~= '' then
-      data = row.last_name
-    else
-      data = {}
-    end
-
     cb({last_name = data})
-
   end)
 
 end)
@@ -127,17 +117,7 @@ end)
 on('esx:player:load:dob', function(identifier, playerId, row, userData, addTask)
 
   addTask(function(cb)
-
-    local data = {}
-
-    if row.dob and row.dob ~= '' then
-      data = row.dob
-    else
-      data = {}
-    end
-
     cb({dob = data})
-
   end)
 
 end)
@@ -145,17 +125,7 @@ end)
 on('esx:player:load:sex', function(identifier, playerId, row, userData, addTask)
 
   addTask(function(cb)
-
-    local data = {}
-
-    if row.sex and row.sex ~= '' then
-      data = row.sex
-    else
-      data = {}
-    end
-
     cb({sex = data})
-
   end)
 
 end)
