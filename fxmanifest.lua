98d9714098d6441f91572633a3989301437e40a3
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2020-05-28 03:06:18 +0200

    bump version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -17,7 +17,7 @@ local esx_config = {
 fx_version      'adamant'
 game            'gta5'
 description     'ESX'
-version         '2.0.0'
+version         '2.0.1'
 ui_page         'hud/index.html'
 ui_page_preload 'yes'
 
